c7ade6bd2940213e6faa3f6b69079d6560659ad5
valeriavicious <valeriavicious13@gmail.com>
2020-04-21 14:06:23 +0800

    Создание игрового окна.

---
+++ Stars_Game/Stars_Game/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Stars_Game
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            Form game_form = new Form();
+            game_form.Width = 800;
+            game_form.Height = 600;
+            game_form.Show();
+
+            Application.Run(game_form);
+        }
+    }
+}
