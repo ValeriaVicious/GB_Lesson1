5c3bfd1ecb9f1b890039199230c912803486b1aa
valeriavicious <valeriavicious13@gmail.com>
2020-04-25 05:41:22 +0800

    Добавлены константы Интервала Таймера и Высоты, Ширины игрового окна

--- Stars_Game/Stars_Game/Game.cs
+++ Stars_Game/Stars_Game/Game.cs
@@ -15,7 +15,7 @@ namespace Stars_Game
 
     internal static class Game
     {
-        private static BufferedGraphicsContext __Context;
+           private static BufferedGraphicsContext __Context;
         private static BufferedGraphics __Buffer;
         private static VisualObject[] __GameObjects;
 
@@ -34,6 +34,176 @@ namespace Stars_Game
             __Buffer = __Context.Allocate(g, new Rectangle(0, 0, Width, Height));
 
             Timer timer = new Timer { Interval = 100 };
+            timer.Tick += OnVimerTick;
+            timer.Start();
+        }
+
+        private static void OnVimerTick(object sender, EventArgs e)
+        {
+            Update();
+            Draw();
+        }
+
+      
+        public static void Draw()
+        {
+
+            Graphics g = __Buffer.Graphics;
+            g.Clear(Color.Black);
+
+            Image newImage = Image.FromFile("universe-2742113_1920.jpg");
+           
+            g.DrawImage(newImage, new RectangleF(0, 0,
+              Width, Height));
+          
+
+            foreach (var game_object in __GameObjects)
+
+                game_object.Draw(g);
+
+            __Buffer.Render();
+
+        }
+
+        public static void Load()
+        {
+            __GameObjects = new VisualObject[30];
+
+            for (int i = 0; i < __GameObjects.Length / 2; i++)
+            {
+                __GameObjects[i] = new VisualObject
+                    (new Point(600, i * 20),
+                    new Point(15 - i, 20 - i),
+                    new Size(20, 20));
+            }
+
+            for (int i = __GameObjects.Length / 2; i < __GameObjects.Length; i++)
+            {
+                __GameObjects[i] = new Star(
+                    new Point(600, (int)i / 2 * 20),
+                    new Point(-i, 0), 30);
+            }
+        }
+
+        public static void Update()
+        {
+            foreach (var game_object in __GameObjects)
+            {
+                game_object.Update();
+            }
+        }
+    }
+}
+namespace Stars_Game
+{
+
+    internal static class Game
+    {
+           private static BufferedGraphicsContext __Context;
+        private static BufferedGraphics __Buffer;
+        private static VisualObject[] __GameObjects;
+
+        public static int Width { get; set; }
+        public static int Height { get; set; }
+     
+
+
+    public static void Initialize(Form form)
+        {
+            Width = form.Width;
+            Height = form.Height;
+
+            __Context = BufferedGraphicsManager.Current;
+            Graphics g = form.CreateGraphics();
+            __Buffer = __Context.Allocate(g, new Rectangle(0, 0, Width, Height));
+
+            Timer timer = new Timer { Interval = 100 };
+            timer.Tick += OnVimerTick;
+            timer.Start();
+        }
+
+        private static void OnVimerTick(object sender, EventArgs e)
+        {
+            Update();
+            Draw();
+        }
+
+      
+        public static void Draw()
+        {
+
+            Graphics g = __Buffer.Graphics;
+            g.Clear(Color.Black);
+
+            Image newImage = Image.FromFile("universe-2742113_1920.jpg");
+           
+            g.DrawImage(newImage, new RectangleF(0, 0,
+              Width, Height));
+          
+
+            foreach (var game_object in __GameObjects)
+
+                game_object.Draw(g);
+
+            __Buffer.Render();
+
+        }
+
+        public static void Load()
+        {
+            __GameObjects = new VisualObject[30];
+
+            for (int i = 0; i < __GameObjects.Length / 2; i++)
+            {
+                __GameObjects[i] = new VisualObject
+                    (new Point(600, i * 20),
+                    new Point(15 - i, 20 - i),
+                    new Size(20, 20));
+            }
+
+            for (int i = __GameObjects.Length / 2; i < __GameObjects.Length; i++)
+            {
+                __GameObjects[i] = new Star(
+                    new Point(600, (int)i / 2 * 20),
+                    new Point(-i, 0), 30);
+            }
+        }
+
+        public static void Update()
+        {
+            foreach (var game_object in __GameObjects)
+            {
+                game_object.Update();
+            }
+        }
+    }
+}
+namespace Stars_Game
+{
+
+    internal static class Game
+    {
+      
+        private const int __TimerInterval = 100;
+
+        private static BufferedGraphicsContext __Context;
+        private static BufferedGraphics __Buffer;
+        private static VisualObject[] __GameObjects;
+
+        public static int Width { get; set; }
+        public static int Height { get; set; }
+     
+
+    public static void Initialize(Form form)
+        {
+            Width = form.Width;
+            Height = form.Height;
+
+            __Context = BufferedGraphicsManager.Current;
+            Graphics g = form.CreateGraphics();
+            __Buffer = __Context.Allocate(g, new Rectangle(0, 0, Width, Height));
+
+            Timer timer = new Timer {Inteval = __TimerInterval};
             timer.Tick += OnVimerTick;
             timer.Start();
         }
--- Stars_Game/Stars_Game/Program.cs
+++ Stars_Game/Stars_Game/Program.cs
@@ -14,9 +14,12 @@ namespace Stars_Game
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
+            const int game_form_width = 800;
+            const int game_form_height = 600;
+
             Form game_form = new Form();
-            game_form.Width = 800;
-            game_form.Height = 600;
+            game_form.Width = game_form_width;
+            game_form.Height = game_form_height;
             game_form.Show();
 
             Game.Initialize(game_form);
