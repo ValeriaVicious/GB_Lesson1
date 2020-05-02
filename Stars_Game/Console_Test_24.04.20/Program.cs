70a6ee8203a6d8c9f4794c18a58ad9d87ef301c6
valeriavicious <valeriavicious13@gmail.com>
2020-05-02 16:22:18 +0800

    Применение делегатов для работы с файлами

--- Stars_Game/Console_Test_24.04.20/Program.cs
+++ Stars_Game/Console_Test_24.04.20/Program.cs
@@ -12,6 +12,7 @@ namespace Console_Test_24._04._20
     /// <param name="str"></param>
     /// <returns></returns>
     internal delegate int StringProcessor(string str);
+    internal delegate void StudentProcessor(Student student);
     class Program
     {
         static void Main(string[] args)
@@ -30,16 +31,16 @@ namespace Console_Test_24._04._20
                     Ratings = rnd.GetValues(rnd.Next(20, 30), 3, 6)
                 });
 
-            foreach (Student student in decanat)
+            /*foreach (Student student in decanat)
             {
                 Console.WriteLine(student.Name);
-            }
+            }*/
 
             Student student_to_remove = decanat[0];
             decanat.Remove(student_to_remove);
 
-            Student random_student = new Student { Name = rnd.GetValue("Иванов", "Шевцов", "Седых", "Гриченко", "Рыбкин") };
-            var random_rating = rnd.GetValue<int>(2, 3, 4, 5);
+            Student random_student = new Student { Surname = rnd.GetValue("Иванов", "Шевцов", "Седых", "Гриченко", "Рыбкин") };
+            /*var random_rating = rnd.GetValue<int>(2, 3, 4, 5);*/
 
             decanat.SaveToFile("decanat.csv");
             Decanat decanat2 = new Decanat();
@@ -48,6 +49,20 @@ namespace Console_Test_24._04._20
             //Создали делегат, который принимает в значение метод
             StringProcessor str_processor = new StringProcessor(GetStringLength);
             int length = str_processor("Hey, Ho, Lets GO!");
+
+            /*StudentProcessor process = new StudentProcessor(PrintStudents);
+            process(random_student);
+            process = RateStudents;//пример, как можно написать, вместо new
+            process(random_student);
+            process = PrintStudents;
+            process(random_student);*/
+
+            ProcessStudents(decanat2, RateStudents);
+            ProcessStudents(decanat2, PrintStudents);
+
+            Decanat decanat3 = new Decanat();
+            ProcessStudents(decanat2, decanat3.Add);//где-то ошибка, список не реализуется
+
             Console.ReadKey();
 
         }
@@ -55,6 +70,25 @@ namespace Console_Test_24._04._20
         private static int GetStringLength(string str)
         {
             return str.Length;
+        }
+
+        private static void PrintStudents(Student student)
+        {
+            Console.WriteLine("[{0}] {1} {2} {3} - {4}", student.Id, student.Surname,
+                student.Name, student.Patronimyc, student.AverageRating);//исключение из-за некорректного формата входящей строки//проблема решена, синтаксическая ошибка
+        }
+
+        private static void RateStudents(Student student)
+        {
+            Random rnd = new Random();
+            student.Ratings.AddRange(rnd.GetValues(5, 2, 6));
+        }
+
+
+        private static void ProcessStudents(IEnumerable<Student> students, StudentProcessor studentProcessor)
+        {
+            foreach (Student student in students)
+                studentProcessor(student);
         }
     }
 }
