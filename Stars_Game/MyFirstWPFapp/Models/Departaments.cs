70b425194f06333551b8df5d86eca984cebd4c14
valeriavicious <valeriavicious13@gmail.com>
2020-05-12 10:48:08 +0800

    - Создание необходимых директорий и расфасовка файлов - Рефакторинг визуальной части приложения

---
+++ Stars_Game/MyFirstWPFapp/Models/Departaments.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstWPFapp.Models
+{
+    class Departaments
+    {
+        public class Departament
+        {
+            public string Name { get; set; }
+            public int Number { get; set; }
+
+            public override string ToString()
+            {
+                return $"{Name}\t{Number}";
+            }
+        }
+
+        void FillComboBox()
+        {
+            //departaments.Add(new Departament() { Number = 1, Name = "Департамент по делам кошачьих лапок" });
+            //departaments.Add(new Departament() { Number = 2, Name = "Департамент по делам пустых мисок" });
+            //departaments.Add(new Departament() { Number = 3, Name = "Департамент по делам с незаконным\nупотреблением валерьянки " });
+            ////lbDepartament.ItemsSource = departaments;
+        }
+
+    }
+}
---
+++ Stars_Game/MyFirstWPFapp/Models/Workers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstWPFapp.Models
+{
+    class Workers
+    {
+        public class Employee
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public int Salary { get; set; }
+
+            public override string ToString()
+            {
+                return $"{Id}\t{Name}\t{Age}\t{Salary}";
+            }
+        }
+
+        //ObservableCollection<Employee> items = new ObservableCollection<Employee>();
+
+        //void FillList()
+        //{
+        //    items.Add(new Employee() { Id = 1, Name = "Кевин", Age = 22, Salary = 3000 });
+        //    items.Add(new Employee() { Id = 2, Name = "Маркус", Age = 28, Salary = 5000 });
+        //    items.Add(new Employee() { Id = 3, Name = "Джейк", Age = 35, Salary = 6000 });
+        //    lbEmployee.ItemsSource = items;
+
+        //}
+    }
+}
