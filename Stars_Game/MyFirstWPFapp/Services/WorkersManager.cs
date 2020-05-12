21f58324079309d4d5d5f14dc41c7dbb76b9891b
valeriavicious <valeriavicious13@gmail.com>
2020-05-12 11:17:25 +0800

    Заполнение класса WorkersManager данными

--- Stars_Game/MyFirstWPFapp/Services/WorkersManager.cs
+++ Stars_Game/MyFirstWPFapp/Services/WorkersManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyFirstWPFapp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,30 @@ namespace MyFirstWPFapp.Services
 {
     class WorkersManager
     {
+        public List<Departaments> Departaments { get; private set; }
+        public WorkersManager()
+        {
+            int workers_id = 1;
+            Departaments = Enumerable.Range(1, 5)
+                .Select(i => new Departaments
+                {
+                    Id = i,
+                    Workers = Enumerable.Range(1, 5).
+                    Select(j => new Workers
+                    {
+                        Id = workers_id,
+                        Name = $"Name {workers_id}",
+                        Surname = $"Surname {workers_id}",
+                        Age = workers_id,
+                        Salary = workers_id
+
+                    }).ToList()
+                })
+                .ToList();
+
+            foreach (Departaments departament in Departaments)
+                foreach (Workers workers in departament.Workers)
+                    workers.Departaments = departament;
+        }
     }
 }
