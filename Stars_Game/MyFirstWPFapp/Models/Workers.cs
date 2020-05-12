eaca071e2d3994b85abc46e95e5ecffd0aa506c8
valeriavicious <valeriavicious13@gmail.com>
2020-05-12 14:43:49 +0800

    - Добавление интерфейса INotifyPropertyChanged к классу Workers - Редактирование свойств с ипсользованием события PropertyChanged

--- Stars_Game/MyFirstWPFapp/Models/Workers.cs
+++ Stars_Game/MyFirstWPFapp/Models/Workers.cs
@@ -1,22 +1,85 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MyFirstWPFapp.Models
 {
-    internal class Workers
+    internal class Workers : INotifyPropertyChanged
     {
+        private string _Name;
+        private string _Surname;
+        private int _Id;
+        private int _Age;
+        private int _Salary;
 
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Surname { get; set; }
-        public int Age { get; set; }
-        public int Salary { get; set; }
 
         public Departaments Departaments { get; set; }
 
+        public event PropertyChangedEventHandler PropertyChanged;
 
+        public int Salary
+        {
+            get => _Salary;
+            set
+            {
+                if (_Salary == value) return;
+                _Salary = value;
+                PropertyChanged?.Invoke(this, new
+                    PropertyChangedEventArgs(nameof(Salary)));
+            }
+        }
+
+        public int Id
+        {
+            get => _Id;
+            set
+            {
+                if (_Id == value) return;
+                _Id = value;
+                PropertyChanged?.Invoke(this, new
+                    PropertyChangedEventArgs(nameof(Id)));
+            }
+
+        }
+        public int Age
+        {
+            get => _Age;
+            set
+            {
+                if (_Age == value) return;
+                _Age = value;
+                PropertyChanged?.Invoke(this, new
+                    PropertyChangedEventArgs(nameof(Age)));
+            }
+        }
+
+        public string Name
+        {
+            get => _Name;
+            set
+            {
+                if (_Name == value) return;
+                _Name = value;
+                PropertyChanged?.Invoke(this, new
+                    PropertyChangedEventArgs(nameof(Name)));
+            }
+        }
+
+        public string Surname
+        {
+            get => _Surname;
+            set
+            {
+                if (_Surname == value) return;
+                _Surname = value;
+                PropertyChanged?.Invoke(this, new
+                    PropertyChangedEventArgs(nameof(Surname)));
+            }
+        }
     }
+
 }
+
