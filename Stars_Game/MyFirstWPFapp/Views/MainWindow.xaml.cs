e39c21bc9c834c5f9db11111ca359a43627628ec
valeriavicious <valeriavicious13@gmail.com>
2020-05-17 15:25:20 +0800

    Проработаны запросы INSERT, UPDATE, DELETE

--- Stars_Game/MyFirstWPFapp/Views/MainWindow.xaml.cs
+++ Stars_Game/MyFirstWPFapp/Views/MainWindow.xaml.cs
@@ -37,12 +37,54 @@ namespace MyFirstWPFapp
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=EmployeeDB;Integrated Security=True;";
+            connection = new SqlConnection(connectionString);
+            dataAdapter = new SqlDataAdapter();
+            SqlCommand command = new SqlCommand("SELECT ID, NAME, SURNAME, SALARY, AGE, DEPARTAMENT FROM Employees", connection);
+            dataAdapter.SelectCommand = command;
 
+
+            //INSERT
+            command = new SqlCommand(@"INSERT INTO Employees (NAME, SURNAME, SALARY, AGE, DEPARTAMENT) 
+            VALUES (@NAME, @SURNAME, @SALARY, @AGE, @DEPARTAMENT); SET @ID = @@IDENTITY;", connection);
+            command.Parameters.Add("@NAME", SqlDbType.NVarChar, -1, "NAME");
+            command.Parameters.Add("@SURNAME", SqlDbType.NVarChar, -1, "SURNAME");
+            command.Parameters.Add("@SALARY", SqlDbType.Int, 0, "SALARY");
+            command.Parameters.Add("@AGE", SqlDbType.Int, 0, "AGE");
+            command.Parameters.Add("@DEPARTAMENT", SqlDbType.NVarChar, -1, "DEPARTAMENT");
+
+            SqlParameter parameter = command.Parameters.Add("@ID", SqlDbType.Int, 0, "ID");
+            parameter.Direction = ParameterDirection.Output;
+            dataAdapter.InsertCommand = command;
+
+            //UPDATE
+            command = new SqlCommand(@"UPDATE Employees SET NAME = @NAME, SURNAME = @SURNAME, SALARY = @SALARY,
+            AGE = @AGE, DEPARTAMENT = @DEPARTAMENT WHERE ID = @ID", connection);
+            command.Parameters.Add("@NAME", SqlDbType.NVarChar, -1, "NAME");
+            command.Parameters.Add("@SURNAME", SqlDbType.NVarChar, -1, "SURNAME");
+            command.Parameters.Add("@SALARY", SqlDbType.Int, 0, "SALARY");
+            command.Parameters.Add("@AGE", SqlDbType.Int, 0, "AGE");
+            command.Parameters.Add("@DEPARTAMENT", SqlDbType.NVarChar, -1, "DEPARTAMENT");
+
+            parameter = command.Parameters.Add("@ID", SqlDbType.Int, 0, "ID");
+            parameter.SourceVersion = DataRowVersion.Original;
+            dataAdapter.UpdateCommand = command;
+
+            //DELETE
+            command = new SqlCommand("DELETE FROM Employees WHERE ID = @ID", connection);//получается, что при удалении я удаляю строку вместе с id, но не удаляю просто данные
+            parameter = command.Parameters.Add("@ID", SqlDbType.Int, 0, "ID");
+            parameter.SourceVersion = DataRowVersion.Original;
+            dataAdapter.DeleteCommand = command;
+            dataTable = new DataTable();
+            dataAdapter.DeleteCommand = command;
+            dataAdapter.Fill(dataTable);
+            WorkersDataGrid.DataContext = dataTable.DefaultView;
         }
 
+        /// <summary> Кнопка создания/добавления данных </summary>
         private void addBtn_Click(object sender, RoutedEventArgs e)
         {
-            DataRow newRow = dataTable.NewRow();
+             DataRow newRow = dataTable.NewRow();
             EditWindow editWindow = new EditWindow(newRow);
             editWindow.ShowDialog();
 
@@ -56,6 +98,7 @@ namespace MyFirstWPFapp
         }
 
 
+        /// <summary> Кнопка удаления данных </summary>
         private void deleteBtn_Click(object sender, RoutedEventArgs e)
         {
             DataRowView rowView = (DataRowView)WorkersDataGrid.SelectedItem;
@@ -63,6 +106,7 @@ namespace MyFirstWPFapp
             dataAdapter.Update(dataTable);
         }
 
+        /// <summary> Кнопка изменения данных </summary>
         private void changeBtn_Click(object sender, RoutedEventArgs e)
         {
             DataRowView rowView = (DataRowView)WorkersDataGrid.SelectedItem;
