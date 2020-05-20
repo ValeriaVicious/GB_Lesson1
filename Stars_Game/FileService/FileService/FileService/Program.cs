25a3155b648a0efe477bee5c3c876364096ab0fb
valeriavicious <valeriavicious13@gmail.com>
2020-05-20 08:33:36 +0800

    Интерфейс сервиса

--- Stars_Game/FileService/FileService/FileService/Program.cs
+++ Stars_Game/FileService/FileService/FileService/Program.cs
@@ -11,43 +11,7 @@ namespace FileHosting
 {
     public class Program
     {
-        static void Main(string[] args)
-        {
-
-            Employee employee = new Employee
-            {
-                Name = "Employee name",
-                Surname = "Employee surname",
-                Birthday = DateTime.Now
-            };
 
 
-            BinaryFormatter formatter = new BinaryFormatter();
-            XmlSerializer xmlSerializer = new XmlSerializer(typeof(Employee));
-
-            using (FileStream bin_file = File.Create("employee.bin"))
-            using (FileStream xml_file = File.Create("employee.xml"))
-            {
-                formatter.Serialize(bin_file, employee);
-                xmlSerializer.Serialize(xml_file, employee);
-            }
-
-            Employee employee1, employee2;
-            using (FileStream bin_file = File.Open("employee.bin", FileMode.Open))
-            using (FileStream xml_file = File.Open("employee.xml", FileMode.Open))
-            {
-                employee1 = (Employee)formatter.Deserialize(bin_file);
-                employee2 = (Employee)xmlSerializer.Deserialize(xml_file);
-            }
-
-        }
-
-        [Serializable]
-        public class Employee
-        {
-            public string Name { get; set; }
-            public string Surname { get; set; }
-            public DateTime Birthday { get; set; }
-        }
     }
 }
